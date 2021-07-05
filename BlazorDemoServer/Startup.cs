2c4705dcb9ca3977b8af7a171624ea39386032f3
Frank Th. van de Ven <fvv@sysdev.nl>
2021-07-05 16:23:50 +0800

    Test push

--- BlazorDemoServer/Startup.cs
+++ BlazorDemoServer/Startup.cs
@@ -19,7 +19,7 @@ namespace Blazor1.Server
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
